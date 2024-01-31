120a08dc77f63ee75cfdacbec11fe89b61881a4f
Adhi Nugroho <adhi.nugroho@email.com>
2024-01-31 21:40:21 +0000

    Formatting display value as percentage

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from bokeh.plotting import show
 hv.extension('bokeh')
 raw = pd.read_csv('data/result.csv')
 edges = raw.sort_values(['source', 'value'])
-sankey = hv.Sankey(edges, label='Personal Finance Planning as Percentage (%)')
+dim = hv.Dimension('value', value_format=lambda x: f'{x} %')
+sankey = hv.Sankey(edges, label='Personal Finance Planning as Percentage (%)',
+                   vdims=dim)
 sankey.opts(label_position='left', edge_color='target',
             node_color='index', cmap='viridis')
 
